d0ec18fbd70f74e854aac38445138d7fe2988b9b
James Anderson <james.p.anderson16@gmail.com>
2017-02-21 17:24:33 -0700

    setup gem files, test structure

---
+++ spec/features/items/user_can_view_items_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+  describe "Visitor can view items" do
+    context "user visits /items" do
+      scenario "user can see all existing items" do
+
+         visit items_path
+
+         expect(page).to have_content("Slammers")
+         expect(page).to have_content("")
+         expect(page).to have_xpath("//img[@src='http://www.the90swereawesome.com/wp-content/uploads/2014/08/intricate.jpg'")
+      end
+    end
+  end
