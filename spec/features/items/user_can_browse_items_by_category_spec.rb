0e6fc106d61fbf74f68d3fd8923e80ef98aee392
Max Glassie <msglassie@gmail.com>
2017-02-21 21:46:58 -0700

    completes visitor can view items functionality

--- spec/features/items/user_can_browse_items_by_category_spec.rb
+++ spec/features/items/user_can_browse_items_by_category_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.feature "Vistor can view items" do
   scenario "they see all items on main page" do
   
-      title: Browsing Items by category
-         body: >
-           Background: I have two categories with titles and each has two different items assigned
-           As a visitor
-           When I visit "/:CATEGORY_NAME"
-           I see all items assigned to that category
+      # title: Browsing Items by category
+      #    body: >
+      #      Background: I have two categories with titles and each has two different items assigned
+      #      As a visitor
+      #      When I visit "/:CATEGORY_NAME"
+      #      I see all items assigned to that category
+  end
+end
--- spec/features/items/user_can_view_items_spec.rb
+++ spec/features/items/user_can_view_items_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
   describe "Visitor can view items" do
     context "user visits /items" do
       scenario "user can see all existing items" do
+          pogs = Category.create(title: "Pogs")
+          item = pogs.items.create(title: "McDonald's Rangers", price: 24.99, description: "McDonald's Power Ranger Coins", image_path: 'power_rangers.jpg', category: pogs)
+          item_2 = pogs.items.create(title: "Power Rangers Packet", price: 30.99, description: "Power Ranger Packet", image_path: 'power_rangers_packet.png', category: pogs)
 
-         visit items_path
+          visit items_path
 
-         expect(page).to have_content("Slammers")
-         expect(page).to have_content("")
-         expect(page).to have_xpath("//img[@src='http://www.the90swereawesome.com/wp-content/uploads/2014/08/intricate.jpg'")
-      end
+          expect(page).to have_content("Pogs")
+
+           expect(page).to have_content("McDonald's Rangers")
+           expect(page).to have_css("img[src*='power_rangers.jpg']")
+
+          expect(page).to have_content("Power Rangers Packet")
+          expect(page).to have_css("img[src*='power_rangers_packet.png']")
     end
   end
+end
