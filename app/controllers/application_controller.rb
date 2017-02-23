7477497b960f44721174f9ebe0a76f5874ce4e2c
James Anderson <james.p.anderson16@gmail.com>
2017-02-22 17:49:47 -0700

    created skeleton for navbar, needs styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
-  helper_method :current_user
+  helper_method :current_user, :all_categories
 
   def set_cart
     @cart = Cart.new(session[:cart])
@@ -21,5 +21,9 @@ class ApplicationController < ActionController::Base
     else
       false
     end
+  end
+
+  def all_categories
+    categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'items#index'
 
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  
   resources :categories do
     resources :items, except: [:index]
   end
