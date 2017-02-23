fab0e5b0e09452a72131e9e221bda4f97a3c31e0
Max Glassie <msglassie@gmail.com>
2017-02-22 22:15:40 -0700

    spikes out test and functionality to remove logic from the view, add methods to the model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,20 @@
 class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
+  # helper_method :item_lookup
+
   def create
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
+    byebug
     session[:cart] = @cart.contents
     flash[:notice] = "You now have #{pluralize(@cart.contents[item.id.to_s], item.title)}."
     redirect_back(fallback_location: items_path)
   end
 
   def show
-    @cart.contents
+    @inventory = @cart.inventory
+    # @cart.contents
   end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -17,4 +17,21 @@ class Cart
   def count_of(item_id)
     contents[item_id.to_s]
   end
+
+
+  def inventory
+    #creates a hash of item objects and their quantity
+    #can pass to the controller
+    @inventory = {}
+    @cart.contents.each do |id, quantity|
+      @inventory[Item.find(id)] = quantity
+    end
+  end
+
+  def sub_total
+    #takes the item price and multiplies it by the quantity from
+    #the inventory hash
+    item.price * @inventory[item]
+  end
+
 end
