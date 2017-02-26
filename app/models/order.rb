eed45ddef02727d6e58869b5ebc52d6e94050057
James Anderson <james.p.anderson16@gmail.com>
2017-02-26 13:08:19 -0700

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,6 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
 
-  def order_item(item)
-    order_items.find_by(item_id: item.id)
-  end
-
   def total_price
     self.items.reduce(0) do |sum, item|
       sum += item.price
