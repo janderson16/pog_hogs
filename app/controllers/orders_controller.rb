617b10fd5af92bba04d7589f71e7723bde4ba9c7
James Anderson <james.p.anderson16@gmail.com>
2017-02-27 14:48:08 -0700

    removed commented out methods in orders_controller that were moved to admin_orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,26 +13,6 @@ class OrdersController < ApplicationController
   end
 
   def show
-    # if current_admin?
-    #   @order = Order.find(params[:id])
-    # else
       @order = current_user.orders.find(params[:id])
-    # end
   end
-
-  # def update
-  #   @order = Order.find(params[:id])
-  #   if @order.update(order_params)
-  #     flash[:success] = "Order successfully updated."
-  #     redirect_back(fallback_location: 'admin/dashboard')
-  #   else
-  #     redirect_back(fallback_location: 'admin/dashboard')
-  #   end
-  # end
-
-  # private
-  #
-  # def order_params
-  #   params.permit(:status, :user_id)
-  # end
 end
