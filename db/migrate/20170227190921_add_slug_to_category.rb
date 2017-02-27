70da3864989328265deb93e4fc4825f4e42abd3c
James Anderson <james.p.anderson16@gmail.com>
2017-02-27 14:15:47 -0700

    Update 20170227190921_add_slug_to_category.rb

--- db/migrate/20170227190921_add_slug_to_category.rb
+++ db/migrate/20170227190921_add_slug_to_category.rb
@@ -1,6 +1,6 @@
--class AddSlugToCategory < ActiveRecord::Migration[5.0]		 +
- -  def change		
- -    add_column :categories, :slug, :string		
- -    add_index :categories, :slug		
- -  end		
- -end
+class AddSlugToCategory < ActiveRecord::Migration[5.0]		 +
+  def change		
+    add_column :categories, :slug, :string		
+    add_index :categories, :slug		
+  end		
+end
