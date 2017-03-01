4ca7de03f0d47b01e954d105db5e93d3a05fdc88
James Anderson <james.p.anderson16@gmail.com>
2017-02-28 22:51:01 -0700

    added audio

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module PogHogs
   class Application < Rails::Application
+    config.assets.precompile << "audio/*"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
