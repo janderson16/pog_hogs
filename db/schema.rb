d92440ef92977ae7b53c8adfce00db139bd729ab
Alex Fosco <alex.hf88@gmail.com>
2017-03-01 23:12:08 -0700

    removes phantom slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302051446) do
+ActiveRecord::Schema.define(version: 20170302061118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20170302051446) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "slug"
-    t.index ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
   end
 
   create_table "items", force: :cascade do |t|
